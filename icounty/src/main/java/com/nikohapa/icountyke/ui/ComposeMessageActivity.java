399ec21fdf95d322f811b781491197eb4fee3ed9
oguya <oguyajames@gmail.com>
2014-06-12 11:28:30 +0300

    ComposeMessage::adding forward message dlg

--- icounty/src/main/java/com/nikohapa/icountyke/ui/ComposeMessageActivity.java
+++ icounty/src/main/java/com/nikohapa/icountyke/ui/ComposeMessageActivity.java
@@ -4,11 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.nikohapa.icountyke.R;
+import com.nikohapa.icountyke.constant.Constants;
+import com.nikohapa.icountyke.ui.frags.ForwardMessageFragment;
 
 public class ComposeMessageActivity extends ActionBarActivity {
+
+    static final String LOG_TAG = "ComposeMessageActivity";
+    static final String ACTIVE_FRAG = "active_frag";
+    private ForwardMessageFragment fragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +57,29 @@ public class ComposeMessageActivity extends ActionBarActivity {
 
             case R.id.action_forward:   //TODO forward to jodongo
                 toast("forwarding");
+                forwardMessage("");
                 break;
 
             default: break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    void forwardMessage(String messageID){
+        fragment = ForwardMessageFragment.newInstance(messageID);
+        fragment.setOnClickListener(ForwardClickListener);
+        fragment.show(getSupportFragmentManager(), ACTIVE_FRAG);
+    }
+
+    AdapterView.OnItemClickListener ForwardClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(ComposeMessageActivity.this, "forwarding to: "+
+                    Constants.INHOUSE_CHAT_MEMBERS[position], Toast.LENGTH_SHORT).show();
+            //TODO forward message
+
+            if(fragment != null) fragment.dismiss();
+            finish();
+        }
+    };
 }
